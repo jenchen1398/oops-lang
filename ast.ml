4bbef52d85f93da1f836d738d5864d2026c04664
Jenny Chen <jc4686@columbia.edu>
2020-04-13 14:24:59 -0700

    added arrays and pipes to ast

--- ast.ml
+++ ast.ml
@@ -15,11 +15,25 @@ type expr =
 type stmt =
 	  Expr of expr
 	| Arr of arr
+        | Redirect of redirect
 	| Block of stmt list
 	| If of expr * stmt * stmt
 	| While of expr * stmt
 	| For of expr * expr * expr * stmt
 	| Return of expr
+
+type arr = 
+ 	 ArrInit of typ * int * string
+       | ArrAssign of string * list
+       | ArrDecl of typ * string * list	
+
+type command = 
+      Command of string * list
+
+type redirect = 
+         InRedirect of command * string
+       | OutRedirect of command * string
+       | AppendRedirect of command * string
 
 type bind = typ * string
 
